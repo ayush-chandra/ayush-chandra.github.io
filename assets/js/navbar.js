3d09046d35b603b3d83cf2956747a3715e5eb973
Ayush Kumar <58789939+ayush-chandra@users.noreply.github.com>
2021-04-15 21:38:29 +0530

    optimized assets

--- assets/js/navbar.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 100) {
-        $('.header-sticky').css('background', 'white');
-    } else {
-        $('.header-sticky').css('background', 'transparent');
-    }
-});
